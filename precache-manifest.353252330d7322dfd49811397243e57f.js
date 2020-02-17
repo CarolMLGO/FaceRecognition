af2981922dd0fcf33a73bdeb798a918bcc2ffbba
Lei_tiger <leifu@oregonstate.edu>
2020-02-16 21:39:57 -0800

    Updates

---
+++ precache-manifest.353252330d7322dfd49811397243e57f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0153a52b23d510f7486865d88183694b",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "c74aa21d877f745da9b8",
+    "url": "/FaceRecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fd6e73a80f3950a25659",
+    "url": "/FaceRecognition/static/css/main.a2eba82b.chunk.css"
+  },
+  {
+    "revision": "c74aa21d877f745da9b8",
+    "url": "/FaceRecognition/static/js/2.88ecdceb.chunk.js"
+  },
+  {
+    "revision": "b2490fbb34248c3403116f401942d851",
+    "url": "/FaceRecognition/static/js/2.88ecdceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd6e73a80f3950a25659",
+    "url": "/FaceRecognition/static/js/main.a2997819.chunk.js"
+  },
+  {
+    "revision": "3f5ec7ca92cced4b35bc",
+    "url": "/FaceRecognition/static/js/runtime-main.e4f51261.js"
+  }
+]);
